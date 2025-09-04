9a7cbbbb8351259c2cc4f9978a0e864d99c2c65c
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-09-04 13:22:24 +0530

    20-error-handling

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -5,9 +5,9 @@ import EventsList from "../components/EventsList";
 function EventsPage() {
   const data = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -19,7 +19,10 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    return { isError: true, message: "Could not fetch events." };
+    // return { isError: true, message: 'Could not fetch events.' };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
   }
