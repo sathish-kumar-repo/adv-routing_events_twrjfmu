02d987f358c6fcad68db4b57081b9a53d97f2143
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-09-04 13:17:18 +0530

    One Way of Error handling (returning instead of throwing)

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -4,6 +4,10 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -11,12 +15,11 @@ function EventsPage() {
 
 export default EventsPage;
 
-// eslint-disable-next-line react-refresh/only-export-components
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    return { isError: true, message: "Could not fetch events." };
   } else {
     return response;
   }
