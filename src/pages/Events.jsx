c515d633774223adc95c4da9bdf9736e9cad0066
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-09-04 13:57:26 +0530

    21-json-helper-function

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -23,6 +23,12 @@ export async function loader() {
     throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
       status: 500,
     });
+    // throw json(
+    //   { message: 'Could not fetch events.' },
+    //   {
+    //     status: 500,
+    //   }
+    // );
   } else {
     return response;
   }
