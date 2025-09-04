79e523d1b8719458342f2688189c7fc8e26b24e6
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-09-04 16:58:37 +0530

    One Way of Sending Data

--- src/pages/NewEvent.jsx
+++ src/pages/NewEvent.jsx
@@ -1,5 +1,34 @@
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return <h1>NewEventPage</h1>;
+  function submitHandler(event) {
+    event.preventDefault();
+
+    /**
+     * And then we could extract data from the form
+
+with help of two-way binding, or refs, for example.
+
+And, we could then manually send our HTDP request here,
+
+maybe manage some loading and error state,
+
+and ultimately navigate
+
+away from this page, once we're done.
+
+We could navigate away with imperative,
+
+and navigation, with help
+
+of that use Navigate Hook I mentioned earlier
+
+in this section.
+     */
+
+    // ! But we have better approach when using React Router
+  }
+  return <EventForm />;
 }
 
 export default NewEventPage;
